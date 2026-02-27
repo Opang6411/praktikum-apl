18e7a7b5294964c322158eb1d8c519ab0eec22ab
Opang6411 <novalarifinnur@gmail.com>
2026-02-27 13:23:03 +0800

    connect

---
+++ kelas/Pertemuan-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string nama;
+    cout << "Hello, World! " << endl;
+    cout << "Masukkan nama kamu: ";
+    getline(cin, nama);
+    cout << "Halo, " << nama;
+return 0;
+}
